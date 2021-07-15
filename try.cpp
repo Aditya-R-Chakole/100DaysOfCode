6c0c15c181245a601f3f9281f25ade21df8a8317
Aditya-R-Chakole <aditya.r.chakole@gmail.com>
2021-07-15 23:25:21 +0530

    Created the Repo

---
+++ try.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main()
+{
+    ll input;
+    cin >> input;
+
+    cout << ((input == 16) ? ("RightCode") : ("WrongCode")) << endl;
+    return 0;
+}
